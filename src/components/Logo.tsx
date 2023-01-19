fe13fd854cdc05c798797706803acfcd4e81dfdf
Grayson Gantek <graysonleegantek@gmail.com>
2023-01-19 11:22:16 -0500

    deprecated prop removal on logo component

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -4,7 +4,7 @@ import { Box } from '@mui/material'
 export function Logo() {
   return (
     <Box sx={{ my: 6, display: 'flex', justifyContent: 'center' }}>
-      <Image src={'/logo.svg'} alt="Logo" width={300} height={110} layout="fixed" priority={true} />
+      <Image src={'/logo.svg'} alt="Logo" width={300} height={110} priority={true} />
     </Box>
   )
 }
