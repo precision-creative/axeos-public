56bad3e624ffc4dbe24eb205cadaeb4bac25feb3
Grayson Gantek <graysonleegantek@gmail.com>
2023-04-25 13:47:17 -0400

    now compresses images before moving them to the server

--- src/pages/api/upload/[room].ts
+++ src/pages/api/upload/[room].ts
@@ -1,5 +1,8 @@
 import { NextApiHandler } from 'next'
 import formidable, { File } from 'formidable'
+import sharp from 'sharp'
+import fs from 'fs/promises'
+import path from 'path'
 
 export type ApiResponse = {
   success: boolean
@@ -25,10 +28,22 @@ const handler: NextApiHandler<ApiResponse> = async (req, res) => {
 
     try {
       const { filepath: filePath, newFilename: fileName } = files.image as File
+      const optimizedFileName = `opt-${fileName}`
+      const optimizedFilePath = path.join(process.cwd(), `uploads/${optimizedFileName}`)
 
+      // Optimize the image as a jpg
+      await sharp(filePath)
+        .resize(630, 630, { fit: 'cover' })
+        .jpeg({ quality: 80 })
+        .toFile(optimizedFilePath)
+
+      // Remove the original image
+      await fs.unlink(filePath)
+
+      // Make the request to the server
       const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/targets/upload/${room}`, {
         method: 'POST',
-        body: JSON.stringify({ filePath, fileName, test: 'two' }),
+        body: JSON.stringify({ filePath: optimizedFilePath, fileName: optimizedFileName }),
         headers: {
           'Content-Type': 'application/json',
         },
@@ -38,15 +53,20 @@ const handler: NextApiHandler<ApiResponse> = async (req, res) => {
         throw new Error('There was an issue with that request on the server.')
       }
 
-      const data = await response.json()
+      await response.json()
     } catch (error) {
-      if (typeof error === 'object' && error !== null && 'message' in error) {
+      if (
+        typeof error === 'object' &&
+        error !== null &&
+        'message' in error &&
+        typeof error.message === 'string'
+      ) {
         res.json({
           success: false,
-          message: 'There was an issue with that request. ' + error.message,
+          message: error.message,
         })
       } else {
-        res.json({ success: false, message: 'There was an issue whith that request.' })
+        res.json({ success: false, message: 'There was an issue with that request.' })
       }
       console.log(error)
       return
